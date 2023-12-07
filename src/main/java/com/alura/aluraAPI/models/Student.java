2c14544866a1207344551c121c0c9aef025d46cd
cref4sp <@>
2023-12-07 10:06:01 -0300

    criado a entidade e classe Student.java.

---
+++ src/main/java/com/alura/aluraAPI/models/Student.java
@@ -0,0 +1,30 @@
+package com.alura.aluraAPI.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+@Entity
+@Table(name = "students")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String typeSignature;
+    //private Set<Curses> completedCurses;
+
+
+}
