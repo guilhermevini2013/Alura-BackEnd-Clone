9da8a41484e3246c69f71b2d54d2c1a2458cf1b5
cref4sp <@>
2023-12-07 09:44:31 -0300

    Initial commit

---
+++ src/main/java/com/alura/aluraAPI/AluraApiApplication.java
@@ -0,0 +1,13 @@
+package com.alura.aluraAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraApiApplication.class, args);
+	}
+
+}
