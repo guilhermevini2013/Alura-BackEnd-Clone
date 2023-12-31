94994611dacea5fcac9608d345d4c4143355dfdb
01nevida <01@vida>
2023-12-31 09:58:47 -0300

    configurado o Security

--- src/main/java/com/alura/aluraAPI/config/security/SpringSecurityConfig.java
+++ src/main/java/com/alura/aluraAPI/config/security/SpringSecurityConfig.java
@@ -1,21 +1,23 @@
-//package com.alura.aluraAPI.config.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SpringSecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http.csrf(csrf-> csrf.disable())
-//                .cors(cors-> cors.disable())
-//                .authorizeHttpRequests(auth-> auth.anyRequest().permitAll())
-//                .build();
-//    }
-//
-//}
+package com.alura.aluraAPI.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf-> csrf.disable())
+                .cors(cors-> cors.disable())
+                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+                .httpBasic(x-> x.setBuilder(http))
+                .build();
+    }
+}
