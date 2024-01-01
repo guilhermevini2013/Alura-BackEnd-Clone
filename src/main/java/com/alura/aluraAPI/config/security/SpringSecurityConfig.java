c8cb34bd10212a4131658eb01aebbb0ac0676504
01nevida <01@vida>
2024-01-01 10:31:43 -0300

    cirado a classe de Configuracao do Spring e configurado as Principais rotas

--- src/main/java/com/alura/aluraAPI/config/security/SpringSecurityConfig.java
+++ src/main/java/com/alura/aluraAPI/config/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.alura.aluraAPI.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +17,16 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf-> csrf.disable())
                 .cors(cors-> cors.disable())
-                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers(HttpMethod.GET,"/course/filter").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/course/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/course").permitAll()
+                        .requestMatchers(HttpMethod.GET,"training/filter").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/training").permitAll()
+                        .requestMatchers(HttpMethod.DELETE,"/course/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/training/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/training").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/course").hasRole("ADMIN"))
                 .httpBasic(x-> x.setBuilder(http))
                 .build();
     }
