b4fe477c2d7a8380acb8184404942ef6178d4341
guilhermevini2013 <guilhermevini2013@hotmail.com>
2024-02-04 17:19:08 -0300

    criado a entidade Category.java

---
+++ src/main/java/com/alura/aluraAPI/models/content/Category.java
@@ -0,0 +1,18 @@
+package com.alura.aluraAPI.models.content;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
